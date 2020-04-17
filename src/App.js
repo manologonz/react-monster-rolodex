895dd5df62b1e4ea8342a99b43cb871b2569492f
Manolo Gonzalez <manologonz203@gmail.com>
2020-04-16 19:19:58 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {CardList} from "./components/card-list/card-list.component"
+import {SearchBox} from "./components/search-box/search-box.component";
 import './App.css';
 
-class App extends Component{
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: ''
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            monsters: [],
+            searchField: ''
+        }
     }
-  }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json()).then(users => {
-          this.setState({monsters: users})
-    })
-  }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json()).then(users => {
+            this.setState({monsters: users})
+        })
+    }
 
-  render() {
-    return (
-        <div className="App">
-          <input
-              type="password"
-              placeholder="Search monsters"
-              onChange={e => this.setState({searchField:e.target.value})}
-          />
-          <CardList monsters={this.state.monsters}/>
-        </div>
-    );
-  }
+    handleChange = e => {
+        this.setState({searchField: e.target.value})
+    }
+
+    render() {
+        const {monsters, searchField} = this.state
+        const filteredMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        )
+        return (
+            <div className="App">
+                <SearchBox
+                    placeholder="Search Monsters"
+                    handleChange={e => this.setState({searchField: e.target.value})}
+                />
+                <CardList monsters={filteredMonsters}/>
+            </div>
+        );
+    }
 }
 
 export default App;
